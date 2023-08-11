6f57a1a05be824d64c903681883c1213fd658d02
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-08-11 18:05:36 +0900

    ネットワークからの情報出力（まだloopには未対応）

--- nc.rb
+++ nc.rb
@@ -1,13 +1,35 @@
 require 'socket'
-host = ARGV[0]
-port = ARGV[1]
+require 'optparse'
 
-puts "DEBUG: host='#{host}' port='#{port}'"
-s = TCPSocket.new "#{host}", "#{port}"
+opt = OptionParser.new
+option = {}
+opt.on('-l') { |l| option[:l] = l }
+argv = opt.parse(ARGV)
 
-loop do
-  string = $stdin.gets
-  s.print(string)
+
+
+
+if option[:l]
+  host = ARGV[1]
+  port = ARGV[2]
+  puts "DEBUG: host='#{host}' port='#{port}'"
+  server = TCPServer.new "#{host}","#{port}"
+  loop do
+    puts "DEBUG: accept..."
+    connection = server.accept
+    puts "DEBUG: accepted"
+    string = connection.gets
+    print string
+  end
+else
+  host = ARGV[0]
+  port = ARGV[1]
+  puts "DEBUG: host='#{host}' port='#{port}'"
+  s = TCPSocket.new "#{host}", "#{port}"
+  loop do
+    string = $stdin.gets
+    s.print(string)
+  end
 end
 
 s.close
