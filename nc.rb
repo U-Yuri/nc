50dc5a2c826430025c6f7ee77504a93d929154a2
yuriririn <80314978+yuriririn@users.noreply.github.com>
2023-08-11 15:04:48 +0900

    オプションなしのncコマンド作成

--- nc.rb
+++ nc.rb
@@ -1,1 +1,13 @@
-aaaa
+require 'socket'
+host = ARGV[0]
+port = ARGV[1]
+
+puts "DEBUG: host='#{host}' port='#{port}'"
+s = TCPSocket.new "#{host}", "#{port}"
+
+loop do
+  string = $stdin.gets
+  s.print(string)
+end
+
+s.close
